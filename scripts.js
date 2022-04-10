b1df6b28070a40a6629abfc04d83a42a1b3bff16
Jorge <fulano@email.com>
2022-04-09 23:32:20 -0300

    layout finalizado, efeitos aplicados, java em dev

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+/*minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
+}*/
+
+
+function selecionarCarta(carta){
+	if(!carta.classList.contains("deuMatch")){
+		carta.classList.toggle("selecionada");
+	}
+
+	
+	/*se outra carta for igual
+	carta.classList.add("deuMatch");
+	*/
+	
 }
