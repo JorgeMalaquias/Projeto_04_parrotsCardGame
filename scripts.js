7c7970d4dc7456a522d10e78e6d27709afea83b1
Jorge <fulano@email.com>
2022-04-10 21:02:24 -0300

    funcionalidades requisitadas cumpridas, necess ajustes no layout

--- scripts.js
+++ scripts.js
@@ -1,23 +1,48 @@
-/*minhaArray.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
-
-// Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
-}*/
+}
+
+let cartasTotais = ["/img/bobrossparrot.gif", "/img/explodyparrot.gif", "/img/fiestaparrot.gif", "/img/metalparrot.gif", "/img/revertitparrot.gif",
+"/img/tripletsparrot.gif", "/img/unicornparrot.gif"];
+cartasTotais.sort(comparador);
+
+
 let numeroDeCartas = Number(prompt("Com quantas cartas você gostaria de jogar?"));
 while(!(numeroDeCartas<=14 && numeroDeCartas>=4 && numeroDeCartas%2===0)){
 	numeroDeCartas = Number(prompt("Com quantas cartas você gostaria de jogar?"));
 }
+
+let cartasDoJogo = [];
+for(let i=0;i<(numeroDeCartas/2);i++){
+	cartasDoJogo.push(cartasTotais[i]);
+	cartasDoJogo.push(cartasTotais[i]);
+}
+cartasDoJogo.sort(comparador);
+
+for(let i=0;i<numeroDeCartas;i++){
+	document.querySelector("ul").innerHTML += `<li onclick="selecionarCarta(this)" class="carta">
+	<div class="back">
+		<img src=${cartasDoJogo[i]} class="figura">
+	</div>
+	<div class="front">
+		<img src="/img/front.png">
+	</div>
+</li>`
+}
+
 let cartasSelecionadas = 0;
 let cartasPareadas = 0;
 let jogadas = 0;
 
+
+
 function selecionarCarta(carta){
 	if((!carta.classList.contains("deuMatch")) && cartasSelecionadas<2){
 		jogadas++;
-		carta.classList.toggle("selecionada");
 		cartasSelecionadas++;
+		carta.classList.toggle("selecionada");
+		console.log("cliquei" + cartasSelecionadas);
 	}
 	if(cartasSelecionadas===2){
 		let img = document.querySelectorAll(".selecionada .figura");
